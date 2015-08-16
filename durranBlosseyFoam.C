d95cc95eedd689ae237266e1a70ec4f32dcdf568
James Shaw <js102@zepler.net>
2015-08-16 18:48:13 +0100

    use controlDict for timing

--- durranBlosseyFoam.C
+++ durranBlosseyFoam.C
@@ -3,12 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-    simpleMatrix<scalar> m(4, 0, 0);
-    Field<scalar>& rhs = m.source();
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #define dt runTime.deltaT()
+
+/*    const scalar x_min = -150e3;
+    const scalar x_max = 150e3;
+    const scalar dx = (x_max - x_min) / 300;
+*/
+
+    while (runTime.loop())
+    {
+    }
+
+    Field<scalar> rhs(4);
     rhs[0] = 0;
     rhs[1] = 2;
     rhs[2] = 4;
     rhs[3] = 6;
+
+    simpleMatrix<scalar> m(4, 0, 0);
+    m.source() = rhs;
 
     for (int i=0; i<4; i++) {
         m[i][i] = 2;
